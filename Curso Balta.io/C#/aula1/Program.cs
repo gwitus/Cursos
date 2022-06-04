0451a5bc29f845311caba7cd5df4dd7de2226cbd
Emanuel <emanuelbarbosati@gmail.com>
2022-06-04 16:45:03 -0400

    Commit 1.0

---
+++ Curso Balta.io/C#/aula1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Aula 1 estrutura dos dados em C#
+
+//1- Importações necessárias no código
+//2- Namespace
+//3- Classe 
+//4- Método principal
+
+//Instruções => Primeira coisa é um dotnet new console
+
+//1
+using System;
+//2
+namespace MeuApp {
+    //3
+    class Program {
+        //4
+        static void Main(string[] args){
+            var texto = "testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
+
+//System é o tipo base do C#
---
+++ Curso Balta.io/C#/aula2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace calculadora {
+    class calculadorafoda{
+        static void Main(string[] args){
+            Console.WriteLine("\n\nSalve lek, isso aqui é uma calculadora\n");
+            double resultado;
+            var numero1 = 0.0;
+            var numero2 = 0.0;
+            int escolha = 0;
+            int contador = 0;
+            string tmp;
+
+            while (contador == 0){
+                Console.WriteLine("\nInsira 2 números em ordem consecultiva para realizar uma operação");
+                tmp = Console.ReadLine();
+                numero1 = double.Parse(tmp);
+
+                tmp = Console.ReadLine();
+                numero2 = double.Parse(tmp);
+
+                Console.WriteLine("Escolha uma operação a ser realizada;\n1 - Soma\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Desligar sistema\n");
+                tmp = Console.ReadLine();
+                escolha = int.Parse(tmp);
+
+                switch (escolha){
+                    case 1:
+                        resultado = numero1 + numero2;
+                        Console.WriteLine("\nA soma das váriaveis resulta em: " + resultado);
+                        Console.WriteLine("\n\n");
+                        break;
+                    
+                    case 2:
+                        resultado = numero1 - numero2;
+                        Console.WriteLine("\nA subtração das váriaveis resulta em: " + resultado);
+                        Console.WriteLine("\n\n");
+                        break;
+                    
+                    case 3:
+                        resultado = numero1 * numero2;
+                        Console.WriteLine("\nA multiplicação das váriaveis resulta em: " + resultado);
+                        Console.WriteLine("\n\n");
+                        break;
+                    
+                    case 4:
+                        resultado = numero1 / numero2;
+                        Console.WriteLine("\nA divisão das váriaveis resulta em: " + resultado);
+                        Console.WriteLine("\n\n");
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Sistema encerrado!\n");
+                        contador ++;
+                        break;
+                }
+            }
+        }
+    }
+    
+}
