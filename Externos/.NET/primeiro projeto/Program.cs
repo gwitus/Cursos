771d9fce89ebdd6cc5e1969b830b2b95d8419829
gwitus <emanuelbarbosati@gmail.com>
2023-09-14 17:55:41 -0300

    Learn news things

--- Externos/.NET/primeiro projeto/Program.cs
+++ Externos/.NET/primeiro projeto/Program.cs
@@ -20,7 +20,41 @@ namespace PrincipalManager {
 
             // Maneira do curso de realizar a parada
             candidato.Nome = "Elon Musk";
-            Console.WriteLine($"Nome: {candidato.Nome});
+            Console.WriteLine($"Nome: {candidato.Nome}");
+
+            // Chamando funções
+            Program.TesteFunction();
+            
+            // Funções com parametros
+            int aux = SomarNumero(1,3);
+            Console.WriteLine(aux); 
+
+            // Chamando de outra forma
+            Console.WriteLine(SomarNumero(1,2));
+
+            //++++++++++Foreach para percorrer+++++++++++++\\
+            numeros.Add(10);
+            numeros.Add(20);
+
+            foreach (var i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+            
         }
+
+        // Aprendendo funções
+        // Estava dando o erro devido ao static não estar configurado
+        // Estrutura de uma função : modificador_retorno TipoDeRetorno NomeDaFuncao(parâmetros){}
+        private static string TesteFunction () {
+            return "Deu certo!";
+        }
+
+        // Funções com parametros
+        public static int SomarNumero(int a, int b){
+            return a + b;
+        }
+
+        private static List<int> numeros = new List<int>();
     }
 }
